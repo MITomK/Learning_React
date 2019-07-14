9fe5953be035c621c167557a88c3ae2132b408f4
TomK <thomas.krech@main-incubator.com>
2019-07-14 21:51:19 +0200

    added KeyboardAvoidingView and editor formatOnSave configuration

--- projects/MyJournal/App.js
+++ projects/MyJournal/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import { FlatList, Picker, StyleSheet, Text, TextInput, View } from 'react-native';
+import React from "react";
+import {
+  FlatList,
+  KeyboardAvoidingView,
+  Picker,
+  StyleSheet,
+  Text,
+  TextInput,
+  View
+} from "react-native";
 
 export default class App extends React.Component {
-
   state = { items: [] };
 
   // Fügt ein Item der liste im State hinzu
@@ -18,35 +25,39 @@ export default class App extends React.Component {
   }
 
   render() {
-    let content = <Text>Keine Einträge im Tagebuch!</Text>
+    let content = <Text>Keine Einträge im Tagebuch!</Text>;
     if (this.state.items.length > 0) {
-      content = <FlatList
-                  style={styles.list}
-                  data={this.state.items}
-                  renderItem={({ item }) => <Text>{item.text}</Text>}
-                  keyExtractor={item => item.date.toString()}  // toString hier wichtig da der keyExtractor denselben erwartet
-                />
+      content = (
+        <FlatList
+          style={styles.list}
+          data={this.state.items}
+          renderItem={({ item }) => <Text>{item.text}</Text>}
+          keyExtractor={item => item.date.toString()} // toString hier wichtig da der keyExtractor denselben erwartet
+        />
+      );
     }
     // console.log("CONTENT IS %o",content);
     return (
       <View style={styles.container}>
         {content}
-        <TextInput
-          style={styles.input}
-          placeholder="Tagebucheintrag erstellen"
-          returnKeyType="done"
-          onSubmitEditing={event => {
-             this._addItem(event.nativeEvent.text)
-          }}
-        // only tried for test purposes
-        // onChangeText={ changedText => {
-        //   console.log("onChangeText: " + changedText);
-        //   this.setState({item: changedText});
-        // }}
-        />
+        <KeyboardAvoidingView behavior="padding">
+          <TextInput
+            style={styles.input}
+            placeholder="Tagebucheintrag erstellen"
+            returnKeyType="done"
+            onSubmitEditing={event => {
+              this._addItem(event.nativeEvent.text);
+            }}
+            // only tried for test purposes
+            // onChangeText={ changedText => {
+            //   console.log("onChangeText: " + changedText);
+            //   this.setState({item: changedText});
+            // }}
+          />
+        </KeyboardAvoidingView>
       </View>
-    )
-  };
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -54,7 +65,7 @@ const styles = StyleSheet.create({
     flex: 1,
     // backgroundColor: '#fff',
     // alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center"
   },
   input: {
     height: 40
