6949287e0b0398d3e8a18c3826ee6dcc8b22f2a4
TomK <thomas.krech@main-incubator.com>
2019-07-19 22:44:44 +0200

    styling stuff

--- projects/MyJournal/components/JournalItemRow.js
+++ projects/MyJournal/components/JournalItemRow.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import {
+  Image,
   Platform,
   StyleSheet,
   Text,
@@ -21,9 +22,15 @@ export default class JournalItemRow extends Component {
     const time = `${hours}:${minutes}:${seconds}`;
     return (
       <TouchableItem>
-        <View>
-          <Text numberOfLines={3}>{item.text}</Text>
-          <Text style={styles.time}>{time}</Text>
+        <View style={styles.container}>
+          <Image
+            style={styles.image}
+            source={require("./../assets/foto.png")}
+          />
+          <View style={styles.itemText}>
+            <Text numberOfLines={3}>{item.text}</Text>
+            <Text style={styles.time}>{time}</Text>
+          </View>
         </View>
       </TouchableItem>
     );
@@ -31,6 +38,21 @@ export default class JournalItemRow extends Component {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    paddingHorizontal: 5,
+    paddingVertical: 3
+  },
+  image: {
+    width: 70,
+    height: 70,
+    marginRight: 5
+  },
+  itemText: {
+    flex: 1,
+    justifyContent: "space-between"
+  },
   time: {
     color: "gray",
     fontSize: 11,
