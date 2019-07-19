da74b9be911c0b33dbda0ce80698bb22c8965c22
TomK <thomas.krech@main-incubator.com>
2019-07-19 20:04:01 +0200

    introduced foto and date on the right side

--- projects/MyJournal/components/JournalItemRow.js
+++ projects/MyJournal/components/JournalItemRow.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import {
   Platform,
+  StyleSheet,
   Text,
   TouchableNativeFeedback,
   TouchableOpacity,
@@ -13,13 +14,27 @@ const TouchableItem =
 export default class JournalItemRow extends Component {
   render() {
     const { item } = this.props;
-
+    const date = new Date(item.date);
+    const hours = date.getHours();
+    const minutes = (date.getMinutes() < 10 ? "0" : "") + date.getMinutes();
+    const seconds = (date.getSeconds() < 10 ? "0" : "") + date.getSeconds();
+    const time = `${hours}:${minutes}:${seconds}`;
     return (
       <TouchableItem>
         <View>
-          <Text>{item.text}</Text>
+          <Text numberOfLines={3}>{item.text}</Text>
+          <Text style={styles.time}>{time}</Text>
         </View>
       </TouchableItem>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  time: {
+    color: "gray",
+    fontSize: 11,
+    fontWeight: "100",
+    alignSelf: "flex-end"
+  }
+});
