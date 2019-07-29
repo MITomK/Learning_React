218de26685f10dabfce1140c26f4f7e10f6197cd
TomK <thomas.krech@main-incubator.com>
2019-07-29 22:48:34 +0200

    tabbar styling

--- projects/MyJournal/AppNavigator.js
+++ projects/MyJournal/AppNavigator.js
@@ -1,42 +1,62 @@
 import React from "react";
-
+import { Platform } from "react-native";
 import { createAppContainer, createBottomTabNavigator } from "react-navigation";
+
+import { SimpleLineIcons } from "@expo/vector-icons";
 
 import JournalScreen from "./screens/JournalScreen";
 import PhotosScreen from "./screens/PhotosScreen";
 import SettingsScreen from "./screens/SettingsScreen";
 
-import { SimpleLineIcons } from "@expo/vector-icons";
-
-const Tabs = createBottomTabNavigator({
-  // const Tabs = new TabNavigator({
-  Journal: {
-    screen: JournalScreen,
-    navigationOptions: {
-      title: "Tagebuch",
-      tabBarIcon: ({ tintColor }) => (
-        <SimpleLineIcons name="book-open" size={24} color={tintColor} />
-      )
+const Tabs = createBottomTabNavigator(
+  {
+    Journal: {
+      screen: JournalScreen,
+      navigationOptions: {
+        title: "Tagebuch",
+        tabBarIcon: ({ tintColor }) => (
+          <SimpleLineIcons name="book-open" size={24} color={tintColor} />
+        )
+      }
+    },
+    Photos: {
+      screen: PhotosScreen,
+      navigationOptions: {
+        title: "Fotos",
+        tabBarIcon: ({ tintColor }) => (
+          <SimpleLineIcons name="picture" size={24} color={tintColor} />
+        )
+      }
+    },
+    Settings: {
+      screen: SettingsScreen,
+      navigationOptions: {
+        title: "Einstellungen",
+        tabBarIcon: ({ tintColor }) => (
+          <SimpleLineIcons name="settings" size={24} color={tintColor} />
+        )
+      }
     }
   },
-  Photo: {
-    screen: PhotosScreen,
-    navigationOptions: {
-      title: "Fotos",
-      tabBarIcon: ({ tintColor }) => (
-        <SimpleLineIcons name="picture" size={24} color={tintColor} />
-      )
-    }
-  },
-  Settings: {
-    screen: SettingsScreen,
-    navigationOptions: {
-      title: "Einstellungen",
-      tabBarIcon: ({ tintColor }) => (
-        <SimpleLineIcons name="settings" size={24} color={tintColor} />
-      )
+  {
+    tabBarPosition: "bottom",
+    tabBarOptions: {
+      activeTintColor: "deepskyblue",
+      inactiveTintColor: "#929292",
+      style: {
+        backgroundColor: "#f4f4f4" // Hintergrundfarbe der Tableiste
+      },
+      indicatorStyle: {
+        height: 0 // Kein Strich am unteren Rand in Android
+      },
+      showIcon: true,
+      upperCaseLabel: false,
+      labelStyle: {
+        // nur Android: kein Abstand nach unten
+        ...Platform.select({ android: { marginBottom: 0 } })
+      }
     }
   }
-});
+);
 
 export default createAppContainer(Tabs);
