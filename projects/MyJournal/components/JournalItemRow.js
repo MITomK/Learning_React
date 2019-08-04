558effba14925ec68e00a7a45df42aa760cd8462
TomK <thomas.krech@main-incubator.com>
2019-08-04 17:47:13 +0200

    first functional gesture implementation

--- projects/MyJournal/components/JournalItemRow.js
+++ projects/MyJournal/components/JournalItemRow.js
@@ -1,8 +1,52 @@
 import React, { Component } from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import {
+  Dimensions,
+  Image,
+  PanResponder,
+  StyleSheet,
+  Text,
+  View
+} from "react-native";
 import TouchableItem from "./TouchableItem";
 
 export default class JournalItemRow extends Component {
+  state = { backgroundColor: "transparent" };
+  componentWillMount() {
+    this._panResponder = PanResponder.create({
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onPanResponderGrant: (evt, gestureState) => {
+        console.log(
+          "PanResponder-Grant: Bewegung beginnt, färbe Hintergrund des Eintrages gelb"
+        );
+        console.log("PanResponder-Grant: färbe Hinergrund rot");
+        this.setState({ backgroundColor: "yellow " });
+      },
+      onPanResponderMove: (evt, gestureState) => {
+        console.log(
+          "PanResponder-Move: falles Bewegung nach links, weitermals die Hälfte des Bildschirms"
+        );
+        console.log("PanResponder-Move: färbe Hinergrund rot");
+        if (gestureState.dx < -(Dimensions.get("window").width / 3)) {
+          this.setState({ backgroundColor: "red" });
+        } else {
+          this.setState({ backgroundColor: "yellow" });
+        }
+      },
+      onPanResponderRelease: (evt, gestureState) => {
+        console.log(
+          "PanResponder-Release: Bewegiung beendet, setze Frabe auf Ursprungsart"
+        );
+        this.setState({ backgroundColor: "transparent" });
+      },
+      onPanResponderTerminate: (evt, gestureState) => {
+        console.log(
+          "PanResponder-Terminate: Bewegung abgebrochen, setze Frabe auf Ursprungsart"
+        );
+        this.setState({ backgroundColor: "transparent" });
+      }
+    });
+  }
+
   render() {
     const { item } = this.props;
     const { text, location, weather } = item;
@@ -17,16 +61,23 @@ export default class JournalItemRow extends Component {
     ) : null;
 
     return (
-      <TouchableItem onPress={this.props.onPress}>
-        <View style={styles.container}>
-          {photo}
-          <View style={styles.itemText}>
-            <Text numberOfLines={3}>{item.text}</Text>
-            <Text style={styles.time}>{`${location || ""} ${weather ||
-              ""} ${time}`}</Text>
+      <View {...this._panResponder.panHandlers}>
+        <TouchableItem onPress={this.props.onPress}>
+          <View
+            style={[
+              styles.container,
+              { backgroundColor: this.state.backgroundColor }
+            ]}
+          >
+            {photo}
+            <View style={styles.itemText}>
+              <Text numberOfLines={3}>{item.text}</Text>
+              <Text style={styles.time}>{`${location || ""} ${weather ||
+                ""} ${time}`}</Text>
+            </View>
           </View>
-        </View>
-      </TouchableItem>
+        </TouchableItem>
+      </View>
     );
   }
 }
